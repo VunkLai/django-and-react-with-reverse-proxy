22d53d12e9f7f2fc42a7e2081d321765dfd3cda3
vunk.lai <vunk.lai@zyxel.com.tw>
2021-11-22 11:08:32 +0800

    [chore] Dockerized backend app

---
+++ backend/run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+until cd /app/server
+do
+    echo "Waiting for server volume..."
+    sleep 5
+done
+
+until ./manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 5
+done
+
+./manage.py collectstatic --no-input
+
+# SQLite doesn't support concurrency.
+# gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
+gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 1 --threads 1
